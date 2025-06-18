9dd42ff9057b41084f3c62f154477b64381c0d40
amandaarod <amanda.rod@uc.cl>
2025-06-18 00:28:21 -0400

    actualizacion

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,10 @@ import Etapa3Bajo from '../views/Etapa3Bajo.vue'
 import Etapa4Alto from '../views/Etapa4Alto.vue'
 import Etapa4Bajo from '../views/Etapa4Bajo.vue'
 import Etapa3Alto from '../views/Etapa3Alto.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
+  { path: '/', component: Home },
   { path: '/bajo-E3', component: Etapa3Bajo },
   { path: '/bajo-E4', component: Etapa4Bajo},
   { path: '/alto-E4', component: Etapa4Alto},
